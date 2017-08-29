187262ee22d3c86205e7015cc4a7385708235b6d
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-08-29 16:15:25 +0530

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,28 @@
+# c-assignment
+// question 1
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+ float b;
+ char c;
+ double d;
+ bool e;
+ cout<<"Enter a integer value : ";
+ cin>>a;
+ cout<<"Enter a float value : ";
+ cin>>b;
+ cout<<"Enter a char value : ";
+ cin>>c;
+ cout<<"Enter a double value : ";
+ cin>>d;
+ cout<<"Enter a bool value : ";
+ cin>>e;
+ cout<<endl<<"integer : "<<a;
+ cout<<endl<<"float : "<<b;
+ cout<<endl<<"char : "<<c;
+ cout<<endl<<"double : "<<d;
+ cout<<"bol : "<<e;
+ return 0;
+}

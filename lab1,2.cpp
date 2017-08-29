5fb2219b49ba458aeb9584ce776216ee075e9041
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-08-29 16:22:51 +0530

    Create lab1,2.cpp

---
+++ lab1,2.cpp
@@ -0,0 +1,13 @@
+# c-assignment
+// question 2
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,b,c;
+ cout<<"Enter two numbers : ";
+ cin>>a>>b;
+ c=a+b;
+ cout<<"The sum of the numbers : "<<c;
+ return 0;
+}

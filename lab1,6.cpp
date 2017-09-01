9648bff49e79ec0cf0c14a3e3d250472f1e20f39
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 16:33:20 +0530

    Create lab1,6.cpp

---
+++ lab1,6.cpp
@@ -0,0 +1,15 @@
+# c-assignment
+// question 6
+#include<iostream>
+using namespace std;
+int main()
+{
+ float r,d,c;
+ cout<<"Enter the radius of the circle : ";
+ cin>>r;
+ d=2*r;
+ c=3.14*d;
+ cout<<"Diameter of the circle : "<<d;
+ cout<<endl<<"Circumference of the circle : "<<c;
+ return 0;
+}

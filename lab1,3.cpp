4f4ec2cdc74378753dada75d41fdcae8b523c639
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 16:28:55 +0530

    Create lab1,3.cpp

---
+++ lab1,3.cpp
@@ -0,0 +1,19 @@
+# c-assignment
+// question 3
+#include<iostream>
+using namespace std;
+int main()
+{
+ float a,b,c;
+ cout<<"Enter two numbers : ";
+ cin>>a>>b;
+ c=a*b;
+ cout<<"Multiplication : "<<c;
+ c=a-b;
+ cout<<endl<<"Subtraction : "<<c;
+ c=a+b;
+ cout<<endl<<"Addition : "<<c;
+ c=a/b;
+ cout<<endl<<"Division : "<<c;
+ return 0;
+} 

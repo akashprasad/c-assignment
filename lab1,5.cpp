55e843c447dce53e2115648c3a899bc30452a4d2
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 16:32:21 +0530

    Create lab1,5.cpp

---
+++ lab1,5.cpp
@@ -0,0 +1,13 @@
+# c-assignment
+question 5
+#include<iostream>
+using namespace std;
+int main()
+{
+ float l,b,a;
+ cout<<"Enter the length and breadth of rectangle : ";
+ cin>>l>>b;
+ a=l*b;
+ cout<<"Area of rectangle : "<<a;
+ return 0;
+}

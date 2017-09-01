e003dee3e8f58a31e45d68db11398dac5944774c
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 18:31:28 +0530

    Update lab1,6.cpp

--- lab1,6.cpp
+++ lab1,6.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 int main()
 {
- float r,d,c;
+ float r,d,c,a;
  cout<<"Enter the radius of the circle : ";
  cin>>r;
  d=2*r;
  c=3.14*d;
+ a=3.14*r*r;
  cout<<"Diameter of the circle : "<<d;
  cout<<endl<<"Circumference of the circle : "<<c;
+ cout<<endl<<"Area of the cricle : "<<a
  return 0;
 }

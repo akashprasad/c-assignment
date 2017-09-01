63dd8a65773a2057375c832ce9c96af96ec7d874
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-01 16:31:28 +0530

    Create lab1,4.cpp

---
+++ lab1,4.cpp
@@ -0,0 +1,13 @@
+# c-assignment
+//  question 4
+#include<iostream>
+using namespace std;
+int main()
+{
+ float l,b,p;
+ cout<<"Enter the length and breadth of rectangle : ";
+ cin>>l>>b;
+ p=2*(l+b);
+ cout<<"Perimeter of rectangle : "<<p;
+ return 0;
+}
